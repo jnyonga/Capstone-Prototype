800b1c09ebb062efa04515363d01086f5cbb658d
“Joel <joel.nyonga@mymail.champlain.edu>
2025-08-28 16:53:54 -0400

    Adjusted wheel collider parameters
    Car tires are more grippy.

--- DeathRacePrototype/Assets/Scripts/CarController.cs
+++ DeathRacePrototype/Assets/Scripts/CarController.cs
@@ -19,7 +19,7 @@ public class CarController : MonoBehaviour
         public Axel axel;
     }
 
-    public float maxAcceleration = 30.0f;
+    public float maxAcceleration = 5.0f;
     public float brakeAcceleration = 50.0f;
 
     public float turnSensitivity = 1.0f;
@@ -62,7 +62,7 @@ public class CarController : MonoBehaviour
     {
         foreach(var wheel in wheels)
         {
-            wheel.wheelCollider.motorTorque = moveInput * 600 * maxAcceleration;
+            wheel.wheelCollider.motorTorque = moveInput * maxAcceleration * 100;
         }
     }
 
