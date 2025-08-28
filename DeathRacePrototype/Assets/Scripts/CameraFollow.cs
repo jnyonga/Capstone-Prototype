d02febf87efc49f0638347015e7ea2fc67ac760b
“Joel <joel.nyonga@mymail.champlain.edu>
2025-08-28 14:07:47 -0400

    Finished Camera Follow script
    Camera follow script attached to main camera

--- DeathRacePrototype/Assets/Scripts/CameraFollow.cs
+++ DeathRacePrototype/Assets/Scripts/CameraFollow.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float moveSmoothness;
+    public float rotSmoothness;
+
+    public Vector3 moveOffset;
+    public Vector3 rotOffset;
+
+    public Transform carTarget;
+
+    private void FixedUpdate()
     {
-        
+        FollowTarget();
+    }
+    void FollowTarget()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+    void HandleMovement()
+    {
+        Vector3 targetPos = new Vector3();
+        targetPos = carTarget.TransformPoint(moveOffset);
+
+        transform.position = Vector3.Lerp(transform.position, targetPos, moveSmoothness * Time.deltaTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    void HandleRotation()
     {
-        
+        var direction = carTarget.position - transform.position;
+        var rotation = new Quaternion();
+
+        rotation = Quaternion.LookRotation(direction + rotOffset, Vector3.up);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotSmoothness * Time.deltaTime);
     }
+
 }
