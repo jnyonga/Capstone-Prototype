13f7ef5ad3d533cb1cd98e521afe40e0362e7863
“Joel <joel.nyonga@mymail.champlain.edu>
2025-08-28 13:51:27 -0400

    Finished Car Controller and started Camera follow script
    Car and turn, accelerate and brake. Wheels are also animated.

--- DeathRacePrototype/Assets/Scripts/CarController.cs
+++ DeathRacePrototype/Assets/Scripts/CarController.cs
@@ -22,7 +22,89 @@ public class CarController : MonoBehaviour
     public float maxAcceleration = 30.0f;
     public float brakeAcceleration = 50.0f;
 
+    public float turnSensitivity = 1.0f;
+    public float maxSteerAngle = 30.0f;
+
+    public Vector3 _centerOfMass;
+
     public List<Wheel> wheels;
 
     float moveInput;
+    float steerInput;
+
+    private Rigidbody carRb;
+
+    void Start()
+    {
+        carRb = GetComponent<Rigidbody>();
+        carRb.centerOfMass = _centerOfMass;
+    }
+    void Update()
+    {
+        GetInputs();
+        Animatewheels();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        Steer();
+        Brake();
+    }
+
+    void GetInputs()
+    {
+        moveInput = Input.GetAxis("Vertical");
+        steerInput = Input.GetAxis("Horizontal");
+    }
+
+    void Move()
+    {
+        foreach(var wheel in wheels)
+        {
+            wheel.wheelCollider.motorTorque = moveInput * 600 * maxAcceleration;
+        }
+    }
+
+    void Steer()
+    {
+        foreach(var wheel in wheels)
+        {
+            if(wheel.axel == Axel.Front)
+            {
+                var _steerAngle = steerInput * turnSensitivity * maxSteerAngle;
+                wheel.wheelCollider.steerAngle = Mathf.Lerp(wheel.wheelCollider.steerAngle, _steerAngle, 0.6f);
+            }
+        }
+    }
+
+    void Brake()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            foreach(var wheel in wheels)
+            {
+                wheel.wheelCollider.brakeTorque = 300 * brakeAcceleration;
+            }
+        }
+        else
+        {
+            foreach(var wheel in wheels)
+            {
+                wheel.wheelCollider.brakeTorque = 0;
+            }
+        }
+    }
+
+    void Animatewheels()
+    {
+        foreach(var wheel in wheels)
+        {
+            Quaternion rot;
+            Vector3 pos;
+            wheel.wheelCollider.GetWorldPose(out pos, out rot);
+            wheel.wheelModel.transform.position = pos;
+            wheel.wheelModel.transform.rotation = rot;
+        }
+    }
 }
