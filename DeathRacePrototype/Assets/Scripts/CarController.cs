a74b1f2cae3486b24cf712296d6b266e302801f8
“Joel <joel.nyonga@mymail.champlain.edu>
2025-08-29 17:48:30 -0400

    Added air movement and roll
    Can roll while airborne with q and e as well as move left in right while in the air

--- DeathRacePrototype/Assets/Scripts/CarController.cs
+++ DeathRacePrototype/Assets/Scripts/CarController.cs
@@ -23,18 +23,23 @@ public class CarController : MonoBehaviour
     }
 
     [Header("Performance")]
-    public float maxAcceleration = 5.0f;
-    public float maxSpeed = 25.0f; // m/s
-    public float brakeAcceleration = 50.0f;
+    public float maxAcceleration = 30.0f;
+    public float maxSpeed = 44.704f; // m/s
+    public float brakeAcceleration = 35.0f;
 
     [Header("Steering")]
     public float turnSensitivity = 1.0f;
     public float maxSteerAngle = 30.0f;
 
     [Header("Jump")]
-    public float jumpForce = 500.0f;
-    public KeyCode jumpKey = KeyCode.E;
-    public float jumpCooldown = 1.0f;
+    public float jumpForce = 15000.0f;
+    public KeyCode jumpKey = KeyCode.F;
+    public float jumpCooldown = 3.0f;
+
+    [Header("Air Control")]
+    public float airSteerForce = 300f;
+    public float airAcceleration = 10f;
+    public bool enableAirAcceleration = true;
 
     [Header("Physics")]
     public Vector3 _centerOfMass;
@@ -76,6 +81,7 @@ public class CarController : MonoBehaviour
         Move();
         Steer();
         Brake();
+        AirControl();
         
     }
 
@@ -199,6 +205,33 @@ public class CarController : MonoBehaviour
             else
             {
                 wheel.wheelEffectObj.GetComponentInChildren<TrailRenderer>().emitting = false;
+            }
+        }
+    }
+
+    void AirControl()
+    {
+        if(!isGrounded)
+        {
+            if(Mathf.Abs(steerInput) > 0.1f)
+            {
+                Vector3 strafeForce = transform.right * steerInput * airAcceleration * 600;
+                carRb.AddForce(strafeForce);
+            }
+            float rollInput = 0f;
+            if (Input.GetKey(KeyCode.E))
+            {
+                rollInput = -1f; // Roll left
+            }
+            if (Input.GetKey(KeyCode.Q))
+            {
+                rollInput = 1f; // Roll right
+            }
+
+            if (Mathf.Abs(rollInput) > 0.1f)
+            {
+                Vector3 rollTorque = transform.forward * rollInput * airSteerForce;
+                carRb.AddTorque(rollTorque);
             }
         }
     }
